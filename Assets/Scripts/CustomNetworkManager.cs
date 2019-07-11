2efd99060645bb33156df0c1e71bc1180fe2fe6f
Maarten <maarten.kroon@ogd.nl>
2019-07-11 20:57:37 +0200

    Enable starting from a different scene than the startmenu

--- Assets/Scripts/CustomNetworkManager.cs
+++ Assets/Scripts/CustomNetworkManager.cs
@@ -1,20 +1,42 @@
-﻿using UnityEngine;
-using UnityEngine.Networking;
-using UnityEngine.UI;
+﻿using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
 
 public class CustomNetworkManager : NetworkManager
 {
-    [SerializeField]
-    InputField networkAddressField;
+    private static NetworkManager _instance;
+
+    protected override void Awake()
+    {
+        // Check if we are the current NetworkManager, if not we destroy ourself. 
+        // This enables starting the game from a different scene than the StartMenu for testing.
+        if (NetworkManager.singleton != null && NetworkManager.singleton != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            base.Awake();
+        }
+    }
+
+    private void Start()
+    {
+        // If we dont start from the StartMenu we host a new game.
+        // This enables starting the game from a different scene than the StartMenu for testing.
+        if (SceneManager.GetActiveScene().buildIndex != 0)
+        {
+            HostGame();
+        }
+    }
 
     public void HostGame()
     {
         StartHost();
     }
 
-    public void JoinGame()
+    public void JoinGame(string networkAdress)
     {
-        networkAddress = networkAddressField.text;
+        this.networkAddress = networkAdress;
         StartClient();
     }
 }
---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    [SerializeField]
+    private InputField _networkAddressField;
+
+    [SerializeField]
+    private CustomNetworkManager _customNetworkManager;
+
+    public void HostGameButtonClicked()
+    {
+        _customNetworkManager.HostGame();
+    }
+
+    public void JoinGameButtonClicked()
+    {
+        _customNetworkManager.JoinGame(_networkAddressField.text);
+    }
+}
