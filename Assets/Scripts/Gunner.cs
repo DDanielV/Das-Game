c8aa462f794085998109d490f153928f2e0dfa1d
Maarten <maarten.kroon@ogd.nl>
2019-07-21 17:00:09 +0200

    Refactor more stuff, fix Torpedo speed and reach.

--- Assets/Scripts/Gunner.cs
+++ Assets/Scripts/Gunner.cs
@@ -4,7 +4,6 @@ public class Gunner : PlayerCharacter
 {
     private void LateUpdate()
     {
-        if (_characterCamera.transform.position.y <= 0.1) { RenderSettings.fog = true; } // (_camera.transform.position.y <= 0) werkt niet goed, kans op een _camera half onderwater maar geen fog
-        else { RenderSettings.fog = false; }
+        RenderSettings.fog = _characterCamera.transform.position.y <= 0.1;
     }
 }
--- Assets/Scripts/Torpedo.cs
+++ Assets/Scripts/Torpedo.cs
@@ -2,15 +2,12 @@
 
 public class Torpedo : MonoBehaviour
 {
-    [SerializeField]
-    private float _startSpeed = 20;
+    [SerializeField] private float _startSpeed = 20;
+    [SerializeField] private float _maxSpeed = 100;
+    [SerializeField] private float _acceleration = 100;
+    [SerializeField] private float _reachInUnityUnits = 500;
+    private Vector3 _startingPoint;
 
-    [SerializeField]
-    private float _maxSpeed = 100;
-
-    [SerializeField]
-    private float _acceleration= 100;
-    
     private Rigidbody _rigidbody;
 
     private void Start()
@@ -19,14 +16,17 @@ public class Torpedo : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody>();
         _rigidbody.velocity = transform.forward * _startSpeed;
 
-        // Destroy the torpedo after 10 seconds, this is enough for them to escape view. 
-        // TODO: Replace this with distance traveled.
-        // Object pooling could be used here but is not really needed.
-        Destroy(gameObject, 10);
+        _startingPoint = transform.position;
     }
 
     private void FixedUpdate()
     {
+        // Destroy the torpedo after it has traveled it's reach. We could use object pooling instead but it's not really needed.
+        if (Vector3.Distance(_startingPoint, transform.position) >= _reachInUnityUnits)
+        {
+            Destroy(gameObject);
+        }
+
         // Increases the speed of the torpedo over time until the max speed is reached.
         _rigidbody.AddForce(transform.forward * _acceleration);
         _rigidbody.velocity = _rigidbody.velocity.magnitude > _maxSpeed ? transform.forward * _maxSpeed : _rigidbody.velocity;
