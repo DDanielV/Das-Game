7c1c1b1cc170566072b6291938e2d6e679660f04
Maarten <maarten.kroon@ogd.nl>
2019-07-23 19:01:04 +0200

    Import Standard Assets, we might delete most of them later

---
+++ Assets/Scripts/Seamine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seamine : ShipBase
+{
+    private void OnTriggerEnter(Collider other)
+    {
+
+    }
+
+    private void Explode()
+    {
+
+    }
+
+    protected override void Die()
+    {
+        throw new System.NotImplementedException();
+    }
+}
