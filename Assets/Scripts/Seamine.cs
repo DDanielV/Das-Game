bb6009d0b91af8e53031014f6ac3c7a010e85205
Maarten <maarten.kroon@ogd.nl>
2019-07-23 20:20:37 +0200

    Add explosion to the seamine

--- Assets/Scripts/Seamine.cs
+++ Assets/Scripts/Seamine.cs
@@ -1,21 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Seamine : ShipBase
 {
+    [SerializeField] private GameObject _explosionPrefab;
+    [SerializeField] private int _explosionDamage = 100;
+
     private void OnTriggerEnter(Collider other)
     {
-
+        other.GetComponentInParent<ShipBase>()?.DoDamage(_explosionDamage);
+        Explode();
     }
 
     private void Explode()
     {
-
+        Instantiate(_explosionPrefab, transform.position, transform.rotation);
+        Destroy(gameObject);
     }
 
     protected override void Die()
     {
-        throw new System.NotImplementedException();
+        Explode();
     }
 }
